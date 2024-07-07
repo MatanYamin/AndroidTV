402972774d2c39477ef224d772f00227b1866adf
Matan Yamin <yamin2211@gmail.com>
2024-07-07 14:00:57 +0300

    First commit

---
+++ RemoteForAndroidTV/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace RemoteForAndroidTV;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ RemoteForAndroidTV/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RemoteForAndroidTV;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}

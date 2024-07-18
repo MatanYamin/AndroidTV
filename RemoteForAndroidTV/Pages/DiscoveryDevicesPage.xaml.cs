c5b3bdde9db91aaf6bcc78e266a6e0f1ba8b0769
Matan Yamin <yamin2211@gmail.com>
2024-07-18 18:28:46 +0300

    fixing hot reload

--- RemoteForAndroidTV/Pages/DiscoveryDevicesPage.xaml.cs
+++ RemoteForAndroidTV/Pages/DiscoveryDevicesPage.xaml.cs
@@ -15,6 +15,10 @@ namespace RemoteForAndroidTV
         {
 
             InitializeComponent();
+            Console.WriteLine("MATAN YAMIN");
+             Console.WriteLine("MATAN YAMIN");
+              Console.WriteLine("MATAN YAMIN");
+               Console.WriteLine("MATAN YAMIN");
 testLang();
             // Handle discovery
             _handleDiscovery = new HandleDiscoveryDevices(this);

8bdff21a07880a272235bd2dbb9e13d7cf342243
Matan Yamin <yamin2211@gmail.com>
2024-07-19 17:10:57 +0300

    test

--- RemoteForAndroidTV/Interfaces/INearbyDevicesFinder.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.ObjectModel;
-
-
-public interface INearbyDevicesFinder
-{
-    Task<bool> StartDevicesFindingAsync();
-    Task StopDevicesFindingAsync();
-    void RedirectToSettings();
-}
